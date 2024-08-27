17b0a1a00fe7f8ef93815435d73119a260cf904b
Justin Scroggins <jscroggins@quovis.io>
2024-08-27 00:29:43 -0400

    Update readme with deployment link, remove unnecessary env.d.ts for env vars, and update vite config and package.json with deploy commands

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/screaming-architecture-boilerplate/', // Replace with your repository name
   plugins: [react()],
   resolve: {
     alias: {
