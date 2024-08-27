0cc1c5a12b1e53f642e1d0b49ed164e6b8e68a62
Justin Scroggins <jscroggins@quovis.io>
2024-08-27 10:18:52 -0400

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/react-screaming-architecture-boilerplate/', 
   plugins: [react()],
   resolve: {
     alias: {
